53833850d089f26c71bcfd5212d89ab275e88869
Andres Grisales <andres.grisalesgg@gmail.com>
2020-09-22 14:55:14 -0500

    Add Get data from a server

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -26,12 +26,17 @@ export class HeroDetailComponent implements OnInit {
 
   getHero(): void{
     const id = +this.route.snapshot.paramMap.get('id');
-    this.heroService.getHeroe(id)
+    this.heroService.getHero(id)
       .subscribe(hero => this.hero = hero);
   }
 
   goBack(): void{
     this.location.back();
+  }
+
+  save(): void{
+    this.heroService.updateHero(this.hero)
+    .subscribe(() => this.goBack());
   }
 
 }
